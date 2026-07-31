90afaf74352675f025540156d918e1690e149658
Pavel812344 <p53350752@gmail.com>
2026-07-31 09:10:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -392,18 +392,18 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     elif query.data == 'take_building':
         if 'current_building' not in context.user_data:
             return
-            building_id = context.user_data['current_building']
-            success, msg, prim = take_building(user.id, building_id)
+        building_id = context.user_data['current_building']
+        success, msg, prim = take_building(user.id, building_id)
 
     keyboard = [
         [InlineKeyboardButton("🏢 Руфить!", callback_data='roof_action')],
         [InlineKeyboardButton("🏠 В меню", callback_data='menu')]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    if success:
-        await query.edit_message_text(msg, reply_markup=reply_markup)
-    else:
-        await query.edit_message_text(msg, reply_markup=reply_markup)
+        if success:
+            await query.edit_message_text(msg, reply_markup=reply_markup)
+        else:
+            await query.edit_message_text(msg, reply_markup=reply_markup)
     
     elif query.data == 'menu':
         await start(update, context, from_menu=True)
